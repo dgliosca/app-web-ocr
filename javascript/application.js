f7e679aa46b8d550b6cfa0165e24f0c7c534f5db
Diego <diego@logicalglue.com>
2014-10-05 12:18:54 +0100

    clean up

--- javascript/application.js
+++ javascript/application.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
-            $('#previewImage').attr('src', e.target.result);
+                $('#previewImage').attr('src', e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
     }
